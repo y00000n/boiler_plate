e7a21b27ec397e2d4224187df130b902ecbbb705
yoonjeong <yoonjeong.jeong@cyberone.kr>
2025-05-11 20:20:06 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,15 +2,36 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const config = require('./config/key')
+
+const bodyParser = require('body-parser')
+const {User} = require('./models/User')
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+
+// application/json
+app.use(bodyParser.json())
+
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://jyj930824:dktQS2OYe3bKrkEZ@boilerplate.5tik7tn.mongodb.net/?retryWrites=true&w=majority&appName=boilerplate'
-
-).then(() => console.log('MongoDB Connected...'))
+mongoose.connect(config.mongoURI)
+  .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('nodejs로 만든 서버')
+})
+
+// Routes
+app.post('/register', async (req, res) => {
+  try {
+      const user = new User(req.body)
+      await user.save()
+      res.status(200).json({ success: true })
+  } catch (err) {
+      res.status(400).json({ success: false, err: err.message })
+  }
 })
 
 app.listen(port, () => {
